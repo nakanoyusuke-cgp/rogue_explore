38c7bfffc2957967d9393f3177d52467186a3b6b
nakano_yusuke_cgp <46030579+nakanoyusuke-cgp@users.noreply.github.com>
2023-02-17 14:36:02 +0900

    setup.pyに依存関係を追加

--- setup.py
+++ setup.py
@@ -4,5 +4,8 @@ from setuptools import setup, find_packages
 setup(
     name="rogue_explore",
     version="0.1.0",
-    packages=find_packages()
+    packages=find_packages(),
+    install_requires=[
+        "gym", "tqdm", "numpy"
+    ],
 )
