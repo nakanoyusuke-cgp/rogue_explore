c976ae3f26e9560c3458702a15bc1ec561ab064a
nakano_yusuke_cgp <46030579+nakanoyusuke-cgp@users.noreply.github.com>
2023-02-19 10:38:16 +0900

    gymインタフェースを実装

--- rogue_explore/wrapper.py
+++ rogue_explore/wrapper.py
@@ -1,4 +1,6 @@
 import gym
+from gym.spaces import Tuple, Discrete, Box
+import numpy as np
 
 from rogue_explore.rogue_explore import RogueExplore, DEFAULT_CONFIG
 from rogue_explore.dungeon_generator import DungeonGenerator
@@ -7,31 +9,47 @@ from rogue_explore.dungeon_generator import DungeonGenerator
 class RogueExploreWrapper(gym.Env):
     def __init__(self, config=None, enable_renderer=True, enable_map_pooling=True):
         if config is None:
-            config = DEFAULT_CONFIG
+            self.config = DEFAULT_CONFIG
+        else:
+            self.config = config
 
-        dg = DungeonGenerator(
+        self.enable_renderer = enable_renderer
+        self.enable_map_pooling = enable_map_pooling
+
+        self.dg = DungeonGenerator(
             place_rations=config['enable_rations_and_hunger'],
             pooling=enable_map_pooling,
             depth=DEFAULT_CONFIG['depth'],
             seed_range=DEFAULT_CONFIG['seed_range']
         )
 
-        env = RogueExplore(dungeon_generator=dg, config=config, render=enable_renderer)
+        self.env = RogueExplore(dungeon_generator=self.dg, config=config, render=enable_renderer)
 
-        action_space = None
-        observation_space = None
+        self.action_space = Discrete(self.env.n_actions)
+        obs_shape = self.env.observation_shape()
+        self.observation_space = Tuple((
+            Box(0, 1, obs_shape[0], np.float32),
+            Box(0, 1, (obs_shape[1],), np.float32),
+        ))
 
     def reset(self, **kwargs):
-        pass
+        return self.env.reset()
 
     def step(self, action):
-        pass
+        return self.env.step(action)
 
     def render(self):
-        pass
+        return self.env.render()
 
     def close(self):
-        pass
+        return self.env.close_renderer()
 
     def seed(self, seed=None):
-        pass
+        """
+        ダンジョン生成器のシードを指定する。
+        "enable_map_pooling"が"False"の場合のみ動作する。
+        "seed=None"の場合何も処理を行わない。
+        :param seed: 生成されるダンジョンのseed
+        """
+        if (seed is not None) and (not self.enable_map_pooling):
+            self.dg.set_seed(seed)
