70c8dc7dbaa9acf78582f9368023fbaa04db1de5
nakano_yusuke_cgp <46030579+nakanoyusuke-cgp@users.noreply.github.com>
2023-02-20 02:27:16 +0900

    gymインタフェースのエントリーポイントを修正

--- rogue_explore/__init__.py
+++ rogue_explore/__init__.py
@@ -3,9 +3,10 @@ from rogue_explore.dungeon_generator import DungeonGenerator
 from rogue_explore.dungeon import Symbols, directions, characters, Dungeon
 
 from gym.envs.registration import register
+from rogue_explore.wrapper import RogueExploreWrapper
 
 
 register(
     id='RogueExplore-v0',
-    entry_point="rogue_explore.rogue_explore:RogueExploreWrapper"
+    entry_point="rogue_explore.wrapper:RogueExploreWrapper"
 )
